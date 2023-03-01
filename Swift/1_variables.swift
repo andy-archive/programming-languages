99be6f94be687d0cfff087b7fa95e5df44dd40fe
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-03-02 00:23:33 +0900

    Add: [Swift] 1_variables - 실수 추가
    - Float, Double 추가
    - 문자열 리터럴로 각 예시 출력

--- Swift/1_variables.swift
+++ Swift/1_variables.swift
@@ -1,9 +1,19 @@
 // 1. 변수
 
 // 1.1. 정수
-
 // 1.1.1. Int : 64비트 정수
-var height : Int = 100
+var MtHallaSeaLevel : Int = 1950
+print("MtHallaSeaLevel: \(MtHallaSeaLevel)")
 
-// 1.1.1. UInt : 64비트 양의 정수
-var height : UInt = 100
+// 1.1.2. UInt : 64비트 양의 정수
+var worldPopulation : UInt = 8000000000
+print("worldPopulation: \(worldPopulation)")
+
+// 1.2. 실수
+// 1.2.1. Float : 32비트 부동소수
+var piFloat : Float = 3.1415
+print("piFloat: \(piFloat)")
+
+// 1.2.2. Double : 63비트 부동소수
+var latitudeSeoul : Double = 37.532600
+print("latitudeSeoul: \(latitudeSeoul)")
