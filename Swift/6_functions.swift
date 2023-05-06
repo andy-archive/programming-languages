63a042c7aaa4720b96c5f41a7f9aaef79bc5f1c7
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-05-06 22:29:12 +0900

    Add: [Swift] 함수 - 스코프 개념 추가
    - 내부/외부 스코프의 변수/함수 접근

--- Swift/6_functions.swift
+++ Swift/6_functions.swift
@@ -8,7 +8,7 @@ func aFunction(_ aParameter: Int) -> Int {
 
     return aReturn
 }
-print(aFunction(3)) // 3
+// print(aFunction(3)) // 3
 
 // 함수의 인수 라벨과 매개변수의 이름(Function Argument Labels and Parameter Names)
 // 함수의 매개변수는 각각 인수 라벨과 매개변수 이름을 갖고 있다.
@@ -21,8 +21,56 @@ func addNumbers(from: Int, to: Int) -> Int {
     }
     return result
 }
-print(addNumbers(from: 1, to: 4)) // 10
+// print(addNumbers(from: 1, to: 4)) // 10
 
 // 인수 라벨의 지정(Specifying Argument Labels)
 
 // 인수 라벨의 생략(Omitting Argument Labels)
+
+
+//: ### 스코프(Scope)
+//: #### 1. 변수 접근
+func printString() -> () {
+    print("--- Function Start! --- ")
+
+    var innerVariable = "INNER VARIABLE"
+
+    let isInnerScope = true
+
+    if isInnerScope {
+        print("isInnerScope : \(isInnerScope)")
+    }
+
+    print("Print innerVariable: \(innerVariable)")
+    print("Print outerVariable: \(outerVariable)")
+
+    print("--- Function End! --- ")
+}
+
+// 상위 스코프에서는 하위 스코프의 변수에 접근이 불가능하다
+// print(isInnerScope) // error: cannot find 'isInnerScope' in scope
+// print(innerVariable) // error: cannot find 'innerVariable' in scope
+
+// 변수가 함수보다 나중에 선언되었어도 전역 변수(global variable)이기에 에러 발생이 되지 않는다
+// 전역 변수는 프로세스의 메모리 구조 중 데이터(data)에 저장된다
+var outerVariable = "OUTER VARIABLE"
+
+// 함수 실행
+// printString()
+
+//: #### 2. 함수 접근
+func outerFunction() -> () {
+    print("--- outerFunction Start! --- ")
+
+    func innerFunction() -> () {
+        print("--- innerFunction Start! --- ")
+    }
+
+    // 같은 스코프에 정의된 함수는 접근 가능하다
+    innerFunction()
+}
+
+// 반면에 내부 스코프에 정의된 함수는 접근 불가능하다
+innerFunction() // error: cannot find 'innerFunction' in scope
+
+// outerFunction()
