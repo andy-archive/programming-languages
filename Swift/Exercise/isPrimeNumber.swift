f04e25314ec15834a3678c3080c51ff430ee021f
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-05-08 00:05:05 +0900

    Update: 각주 해제 -> [Swift] 연습 문제 - 소수 문제 판별

--- Swift/Exercise/isPrimeNumber.swift
+++ Swift/Exercise/isPrimeNumber.swift
@@ -26,33 +26,33 @@ if isPrimeNumber(input) {
 }
 
 // 풀이 2: guard 문
-//func isPrimeNumber(_ number: Int) -> Bool {
-//    guard number > 1 else {
-//        return false
-//    }
-//    for i in 2..<number {
-//        if number % i == 0 {
-//            return false
-//        }
-//    }
-//    return true
-//}
+func isPrimeNumber(_ number: Int) -> Bool {
+   guard number > 1 else {
+       return false
+   }
+   for i in 2..<number {
+       if number % i == 0 {
+           return false
+       }
+   }
+   return true
+}
 
 // 풀이 3: switch 문
-//func isPrimeNumber(_ number: Int) -> Bool {
-//    switch number {
-//    case 2, 3:
-//        return true
-//    case let x where x < 2 || x % 2 == 0:
-//        return false
-//    case 5..<Int.max:
-//        for i in stride(from: 3, through: Int(sqrt(Double(x))), by: 2) {
-//            if x % i == 0 {
-//                return false
-//            }
-//        }
-//        return true
-//    default:
-//        return false
-//    }
-//}
+func isPrimeNumber(_ number: Int) -> Bool {
+   switch number {
+   case 2, 3:
+       return true
+   case let x where x < 2 || x % 2 == 0:
+       return false
+   case 5..<Int.max:
+       for i in stride(from: 3, through: Int(sqrt(Double(x))), by: 2) {
+           if x % i == 0 {
+               return false
+           }
+       }
+       return true
+   default:
+       return false
+   }
+}
